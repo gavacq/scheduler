620c46bf2fbd71d99cf1834e51081713ca4dc94e
geecrypt <gavina352@gmail.com>
2021-09-07 22:32:13 -0700

    test Appointments render

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -12,11 +12,13 @@ import {render} from "@testing-library/react";
 /*
   We import the component that we are testing
 */
-import Application from "components/Application";
+import Appointment from "components/Appointment";
 
-/*
+describe("Appointment", () => {
+  /*
   A test that renders a React Component
 */
-it("renders without crashing", () => {
-  render(<Application />);
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
 });
